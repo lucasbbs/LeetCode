c6afc9cabcc7b8dc1c756e774271ca34fb27a9d8
Lucas Braga <lucasbbs@live.fr>
2025-08-24 14:52:13 -0400

    [222] [Time Beats: 100.00%] [Memory Beats: 82.86%] - LeetPush

--- Count-Complete-Tree-Nodes.js
+++ Count-Complete-Tree-Nodes.js
@@ -11,6 +11,14 @@
  * @return {number}
  */
 var countNodes = function(root) {
-    if (root === null) return 0
-    return 1 + countNodes(root.left) + countNodes(root.right)
-}
+    let currentCount = 0
+    if (root === null) return currentCount
+    const count = (root) =>{
+    currentCount++
+    if (root.left) count(root.left)
+    if (root.right) count(root.right)
+    return
+    }
+    count(root)
+    return currentCount
+};
